f8a43184a50dfbe35bfaadc8f78614e0397dc901
mikkelswartz <62695198+mikkelswartz@users.noreply.github.com>
2022-08-10 16:53:00 +0200

    Update main-streamlit.py

--- main-streamlit.py
+++ main-streamlit.py
@@ -99,11 +99,12 @@ if __name__ == "__main__":
             
             #driver.find_element_by_name('password').send_keys(password + Keys.ENTER)
             driver.find_element(by=By.NAME, value = 'password').send_keys(password + Keys.ENTER)
-
+            
             # relocate to orders when page is loaded
             #wait_for_loading(driver, "sidebar-nav")
             #WebDriverWait(driver, 10).until(lambda x: x.find_elements(by=By.CLASS_NAME, value="sidebar-nav"))
             #driver.implicitly_wait(timer)
+            time.sleep(5)
             driver.get(url+"/orders")
             time.sleep(5)
             st.write(driver.current_url)
