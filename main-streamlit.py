03c2d5aab736bea6d02498b011a1523f79fab2c1
mikkelswartz <62695198+mikkelswartz@users.noreply.github.com>
2022-08-14 22:07:44 +0200

    Update main-streamlit.py

--- main-streamlit.py
+++ main-streamlit.py
@@ -17,13 +17,13 @@ import time
 
 options = Options()
 options.add_argument("--headless")
-#options.add_argument("--no-sandbox")
-#options.add_argument("--disable-extensions")
-#options.add_argument("--disable-dev-shm-usage")
-#options.add_argument("--disable-gpu")
-#options.add_argument("--disable-features=NetworkService")
-#options.add_argument("--window-size=1920x1080")
-#options.add_argument("--disable-features=VizDisplayCompositor")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-extensions")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")
+options.add_argument("--disable-features=NetworkService")
+options.add_argument("--window-size=1920x1080")
+options.add_argument("--disable-features=VizDisplayCompositor")
 
 
 
